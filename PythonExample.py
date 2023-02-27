2e3e0cb2afbc0659e044aff615887041998a7122
amiller <acmiller238@gmail.com>
2023-02-26 17:32:32 -0700

    Updated fixed code

--- PythonExample.py
+++ PythonExample.py
@@ -1,12 +1,22 @@
 print("Printing every item in a list")
 
-array = [1, 2, 3, 4, 5, 6]
-new_array = []
+def addArray (array):
+    new_array = []
+    for item in array:
+        print("Working on: " + str(item))
+        add = item + 14
+        print (str(item) + " + 14 = " + str(add))
+        new_array.append(add)
 
-for item in array:
-    print("Working on: " + str(item))
-    add = item + 14
-    print (str(item) + " + 14 = " + str(add))
-    new_array.append(add)
+        print("New array: ", new_array)
+    return new_array
 
-print("New array: ", new_array)
+array_example = [1, 2, 3, 4, 5, 6]
+
+#addArray(array_example)
+
+
+str = "Hello World"
+
+str = str.replace("Hello", "Goodbye")
+print(str)
